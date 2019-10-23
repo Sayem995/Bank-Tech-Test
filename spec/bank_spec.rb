ba891aa3600fcf71bf06d795b95f30c988d95fc6
Sayem995 <sayem.bokhari.1@gmail.com>
2019-10-23 17:31:49 +0100

    write second failing test

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -1,8 +1,14 @@
 require 'bank'
 
 describe "Bank Account" do
+
   subject(:bank_account) { Bank.new }
   it 'has initial balance of 0' do
     expect(subject.balance).to eq(0)
   end
+
+  it 'can deposit money' do
+    expect(subject.deposit).to eq(1000)
+  end
+
 end
