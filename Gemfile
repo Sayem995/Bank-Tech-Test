56e17bb6481b74c67bcbffb02888a1f886f3284c
Sayem995 <sayem.bokhari.1@gmail.com>
2019-10-23 17:06:14 +0100

    first failing test pass

--- Gemfile
+++ Gemfile
@@ -1,1 +1,5 @@
+source 'https://rubygems.org'
+
 gem 'rspec'
+gem 'rubocop', '~> 0.59.0'
+gem 'simplecov', '~> 0.12.0'
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -1,8 +1,8 @@
-require 'Bank'
+require 'bank'
 
 describe "Bank Account" do
-  subject(:bank_account){Bank.new}
+  subject(:bank_account) { Bank.new }
   it 'has initial balance of 0' do
     expect(subject.balance).to eq(0)
   end
-end 
+end
